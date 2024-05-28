b919b7c9931c5ee14021b6cdb43eabad372ea3ad
Aurelien <afocant@s19>
2024-05-28 16:54:33 +0200

    first draft of getting path env

---
+++ main.c
@@ -0,0 +1,37 @@
+#include "pipex.h"
+
+void	ft_get_path(char *envp[])
+{
+	size_t	i;
+	char	**envs;
+	char	**paths;
+
+	i = 0;
+	while (envp[i])
+	{
+		envs = ft_split(envp[i], '=');
+		if (ft_strncmp(envs[0], "PATH", 256) == 0)
+		{
+			paths = ft_split(envs[1], ':');	
+			break;
+		}
+		i++;
+	}
+	i = 0;
+	while (paths[i])
+	{
+		printf("%s\n", paths[i]);
+		i++;
+	}
+}
+
+int main(int argc, char *argv[], char *envp[])
+{
+	if (argc != 5)
+	{
+		errno = 22;
+		perror("You need 4 command line arguments !");		
+		exit(errno);
+	}
+	ft_get_path(envp);
+}
